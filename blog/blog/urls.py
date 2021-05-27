8bea29232b040d881718df9df2a8a979ba67adb3
FXHLL <1745955087@qq.com>
2021-05-27 20:54:33 +0800

    111

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+
+# import logging
+# logger=logging.getLogger('django')
+#
+# def log(request):
+#
+#     logger.info('info')
+#     return HttpResponse("test")
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('',include(('users.urls','users'),namespace='users'))
+    # path('',log),
 ]

32b9f004eb70067d837fa8ac1d46f88ebda2701c
FXHLL <1745955087@qq.com>
2021-06-01 19:23:17 +0800

    222

--- blog/users/models.py
+++ blog/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+
+    mobile=models.CharField(max_length=11,unique=True,blank=False)
+    # 头像
+    avatar=models.ImageField(upload_to='avatar/%Y%m%d/',blank=True)
+    # 简介
+    user_desc=models.CharField(max_length=500,blank=True)
+
+    # 修改默认的字段为号码
+    USERNAME_FIELD = 'mobile'
+    class Meta:
+        db_table='tb_users'
+        verbose_name='用户管理'
+        verbose_name_plural=verbose_name
+
+        def _str_(self):
+            return self.mobile
--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,9 +1,24 @@
 #进行users子应用的视图路由
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView,ImageCodeView
+from users.views import SmsCodeView,LoginView
+from users.views import LogoutView,ForgetPasswordView
+from users.views import UserCenterView
 urlpatterns = [
-    #path第一个参数:路由
+    #path第一个参数:路由,path第二个参数:视图函数名
     path('admin/',RegisterView.as_view()),
-    #path第二个参数:视图函数名
+
     path('register/',RegisterView.as_view(),name='register'),
+    # 图片验证码路由
+    path('imagecode/',ImageCodeView.as_view(),name='imagecode'),
+    # 短信路由
+    path('smscode/',SmsCodeView.as_view(),name='smscode'),
+    #登陆路由
+    path('login/',LoginView.as_view(),name='login'),
+    # 退出登录
+    path('logout/', LogoutView.as_view(), name='logout'),
+    # 忘记密码
+    path('forgetpassword/',ForgetPasswordView.as_view(),name='forgetpassword'),
+
+    path('center/', UserCenterView.as_view(), name='center'),
 ]
